ae3ea40758336abc56bf3c1dda75d92a00f6d1ac
Antonin Cuenot <acw804@outlook.fr>
2024-02-29 13:51:58 +0100

    lastcommit

--- api/components/ForumTheme.php
+++ api/components/ForumTheme.php
@@ -25,29 +25,3 @@ try {
     echo "Erreur lors de l'exécution de la requête : " . $e->getMessage();
 }
 ?>
-<style>
-/* Style pour le conteneur des thèmes */
-div.themes-container {
-    display: flex;
-    flex-wrap: wrap;
-}
-
-/* Style pour chaque thème */
-div.themes-container a {
-    width: calc(33.33% - 20px); /* Environ 33.33% de la largeur du conteneur moins les marges */
-    margin: 10px;
-    background-color: #f0f0f0;
-    padding: 10px;
-    border-radius: 5px;
-    text-align: center;
-    box-shadow: 0 4px 6px rgba(0, 0, 0, 0.6);
-}
-
-/* Style pour les liens dans les thèmes */
-div.themes-container a {
-    text-decoration: none;
-    color: #333;
-    font-weight: bold;
-    text-transform: uppercase;
-}
-</style>
