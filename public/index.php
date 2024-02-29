4c6f270206992f710ac839f2111b4d5bcb62818c
Olivier Allegret <olivier@colnem.net>
2024-02-29 11:22:16 +0100

    29/02 11:22

--- public/index.php
+++ public/index.php
@@ -1,19 +1,7 @@
 <?php
 session_start(); // Démarrage de la session
 
-if (isset($_SESSION['id'])) { // Vérifie si l'utilisateur est connecté
-    require_once('../api/config/bdd.php'); // Inclusion du fichier de configuration de la base de données
-
-    $s = $cnx->prepare("SELECT * FROM utilisateur WHERE id = ?"); // Préparation de la requête SQL pour récupérer l'utilisateur par ID
-    $s->execute([$_SESSION['id']]); // Exécute la requête avec l'ID de l'utilisateur stocké dans la session
-    $r = $s->fetch(); // Récupère la première ligne de résultat
-
-    if ($r && is_array($r) && isset($r['id'])) { // Vérifie si un utilisateur correspondant à l'ID existe
-        $_SESSION['id'] = $r['id']; // Stocke l'ID de l'utilisateur dans la session
-        $_SESSION['pseudo'] = $r['pseudo']; // Stocke le pseudo de l'utilisateur dans la session
-        $_SESSION['email'] = $r['mail']; // Stocke l'adresse e-mail de l'utilisateur dans la session
-    }
-} elseif (isset($_COOKIE['forum_user_token'])) { // Si l'utilisateur n'est pas connecté mais qu'un cookie contenant le jeton est présent
+if (!isset($_SESSION['id']) && isset($_COOKIE['forum_user_token'])) { // Si l'utilisateur n'est pas connecté mais qu'un cookie contenant le jeton est présent
     require_once('../api/config/bdd.php'); // Inclusion du fichier de configuration de la base de données
 
     $s = $cnx->prepare("SELECT * FROM utilisateur WHERE jeton = ?"); // Préparation de la requête SQL pour récupérer l'utilisateur par jeton
@@ -32,8 +20,9 @@ $title = "Mon forum"; // Titre de la page
 $description = "Ceci est une API de gestion de forum"; // Description de la page
 
 define('API', '../api/components/'); // Définition du chemin vers les composants de l'API
+define('INC', '../includes/'); // Définition du chemin vers les composants de l'API
 define('ROOT', '../'); // Définition du chemin vers la racine du site
-require_once(ROOT . "includes/top.php"); // Inclusion du fichier top.php contenant le code HTML de l'en-tête de la page
+require_once(INC . "front_top.php"); // Inclusion du fichier top.php contenant le code HTML de l'en-tête de la page
 ?>
 
 <link rel="stylesheet" href="<?= ROOT; ?>assets/css/style.css"> <!-- Inclusion du fichier CSS -->
@@ -42,7 +31,7 @@ require_once(ROOT . "includes/top.php"); // Inclusion du fichier top.php contena
 <body>
 
 <?php
-require_once(ROOT . "includes/menu.php"); // Inclusion du fichier menu.php contenant le code HTML du menu de navigation
+require_once(INC . "front_menu.php"); // Inclusion du fichier menu.php contenant le code HTML du menu de navigation
 
 $action = isset($_GET['action']) ? $_GET['action'] : ""; // Récupération de la valeur de la variable d'action depuis l'URL
 $type = isset($_GET['type']) ? $_GET['type'] : ""; // Récupération de la valeur du type depuis l'URL
@@ -54,10 +43,10 @@ switch ($action) { // Switch sur la variable d'action
         // echo $_COOKIE['remember_forum_key'];
         break;
     case 'login':
-        require_once(API . "ForumLoginForm.php"); // Inclusion du formulaire de connexion au forum
+        require_once(INC . "forum_user_form_login.php"); // Inclusion du formulaire de connexion au forum
         break;
-    case 'newusers':
-        require_once(API . "ForumUserForm.php"); // Inclusion du formulaire d'inscription de nouveaux utilisateurs
+    case 'newUsers':
+        require_once(INC . "forum_user_form_create.php"); // Inclusion du formulaire d'inscription de nouveaux utilisateurs
         break;
     case 'topics': 
         require_once(API . "ForumTheme.php");
@@ -66,14 +55,14 @@ switch ($action) { // Switch sur la variable d'action
         // page utilisateur
         switch ($type) { // Switch sur le type
             default:
-                require_once(API . "ForumUsersFind.php"); // Inclusion de la page de recherche d'utilisateurs
+                require_once(INC . "forum_user_form_find.php"); // Inclusion de la page de recherche d'utilisateurs
                 break;
             case 'edit':
-                require_once(API . "ForumUserForm.php"); // Inclusion du formulaire de modification d'utilisateur
+                require_once(INC . "forum_user_form_edit.php"); // Inclusion du formulaire de modification d'utilisateur
                 break;
         }
         break;
 }
 
-require_once(ROOT . "includes/bottom.php"); // Inclusion du fichier bottom.php contenant le code HTML du pied de page
+require_once(INC . "front_bottom.php"); // Inclusion du fichier bottom.php contenant le code HTML du pied de page
 ?>
